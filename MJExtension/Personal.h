dde14f75af2fd4532ff27f3be62583fec26cccb5
ygj <ygj@163.com>
2016-07-10 13:01:16 +0800

    添加person

---
+++ MJExtension/Personal.h
@@ -0,0 +1,13 @@
+//
+//  Personal.h
+//  MJExtensionExample
+//
+//  Created by Yazi on 16/7/10.
+//  Copyright © 2016年 小码哥. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Personal : NSObject
+
+@end
---
+++ MJExtension/Personal.m
@@ -0,0 +1,13 @@
+//
+//  Personal.m
+//  MJExtensionExample
+//
+//  Created by Yazi on 16/7/10.
+//  Copyright © 2016年 小码哥. All rights reserved.
+//
+
+#import "Personal.h"
+
+@implementation Personal
+
+@end
